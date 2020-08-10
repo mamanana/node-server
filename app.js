9faed194b5a3843917745db8bf28fcd0713b8ba7
hung <maiquochung26@gmail.com>
2020-08-11 01:58:22 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 const app = express();
+
+// Middlewares
+
+// Import Routers
+
+const postRouter = require('./routers/posts')
+
+app.use('/posts', postRouter)
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
+
+// Connect DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to db'))
+
 // Server listen
 app.listen(3000)
